67cc200c43367f01c614fb6e22b7e2cd5f14a64c
David Kar <kardav2019@gmail.com>
2024-02-06 10:31:33 +0200

    Fix a Bug

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -3,7 +3,7 @@ import { collection, onSnapshot } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import db from '../utils/firebase';
 
-interface experience {
+interface ExperienceProps {
   key: string;
   company: string;
   position: string;
@@ -13,13 +13,13 @@ interface experience {
 }
 
 const Experience = () => {
-  const [experiences, setExperiences] = useState<experience[]>([]);
+  const [experiences, setExperiences] = useState<ExperienceProps[]>([]);
   const [active, setActive] = useState(0);
 
   useEffect(
     () =>
       onSnapshot(collection(db, 'experiences'), (snapshot) => {
-        const data: any = snapshot.docs.map((doc) => ({
+        const data: any[] = snapshot.docs.map((doc) => ({
           ...doc.data(),
           key: doc.id,
         }));
@@ -40,37 +40,41 @@ const Experience = () => {
           This is my experience so far in the industry
         </p>
       </header>
-      <div className="flex justify-center space-x-9 w-10/12 flex-1">
-        <div className="w-1/4">
-          {experiences.map((experience, id) => (
-            <div
-              key={id}
-              className={clsx(
-                'px-2 py-4 text-xl font-bold border-l-4 transition-colors duration-300 ease-in cursor-pointer',
-                id == active
-                  ? 'border-orange-500 text-orange-500'
-                  : 'border-gray-400 text-gray-400'
-              )}
-              onClick={() => setActive(id)}
-            >
-              {experience.company}
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col items-start flex-3 w-3/4">
-          <h3 className="font-bold text-lg">{experiences[active].position}</h3>
-          <span className="text-gray-400">
-            {experiences[active].startDate} - {experiences[active].endDate}
-          </span>
-          <div className="flex flex-col space-y-3 mt-5">
-            {experiences[active].achievements.map((achievement, idx) => (
-              <p key={idx} className="">
-                {achievement}
-              </p>
+      {experiences.length && (
+        <div className="flex justify-center space-x-9 w-10/12 flex-1">
+          <div className="w-1/4">
+            {experiences?.map((experience, id) => (
+              <div
+                key={id}
+                className={clsx(
+                  'px-2 py-4 text-xl font-bold border-l-4 transition-colors duration-300 ease-in cursor-pointer',
+                  id == active
+                    ? 'border-orange-500 text-orange-500'
+                    : 'border-gray-400 text-gray-400'
+                )}
+                onClick={() => setActive(id)}
+              >
+                {experience.company}
+              </div>
             ))}
           </div>
+          <div className="flex flex-col items-start flex-3 w-3/4">
+            <h3 className="font-bold text-lg">
+              {experiences[active].position}
+            </h3>
+            <span className="text-gray-400">
+              {experiences[active]?.startDate} - {experiences[active]?.endDate}
+            </span>
+            <div className="flex flex-col space-y-3 mt-5">
+              {experiences[active]?.achievements?.map((achievement, idx) => (
+                <p key={idx} className="">
+                  {achievement}
+                </p>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
